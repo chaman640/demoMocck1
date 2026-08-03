ab995271c2bee8e70153fc720216a43d4325a5b5
chaman640 <anuj.mishhra420b6r9@gmail.com>
2026-08-03 08:04:02 +0530

    add forget link in page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -240,6 +240,11 @@ const Login = () => {
             <Link to="/Singup" className="text-[#2563EB] font-bold hover:underline ml-1">
               Sign Up
             </Link>
+            <div className="text-right -mt-2 mb-2">
+  <Link to="/ForgotPassword" className="text-xs text-[#2563EB] hover:underline">
+    Password bhool gaye?
+  </Link>
+</div>
           </div>
           
         </div>
